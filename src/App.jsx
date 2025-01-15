5d56c4b90172a633c8785ee944f08e118e1dc2e3
Luma2001 <lumaqui2001@yahoo.com.ar>
2025-01-15 10:57:46 -0300

    [cfg] test 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Próximamente FullMovies</h1>
-      <h2>NO TE LO PIERDAS!!!😎</h2>
-      <h2>LAS MEJORES PELIS</h2>
-      <h2>SAGAS COMPLETAS DE TUS HISTORIAS FAVORITAS</h2>
-    </div>
+    <>
+        <div>
+          <h1>Próximamente FullMovies</h1>
+          <h2>NO TE LO PIERDAS!!!😎</h2>
+          <h2>LAS MEJORES PELIS</h2>
+          <h2>SAGAS COMPLETAS DE TUS HISTORIAS FAVORITAS</h2>
+        </div>
+    </>
+
   )
 }
 
