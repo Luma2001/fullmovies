6ced28752e132f20e4b5a968c1a768bcb6f500cb
Luma2001 <lumaqui2001@yahoo.com.ar>
2025-01-15 10:43:08 -0300

    [cfg] test deploy qa

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <h1>Próximamente FullMovies</h1>
       <h2>NO TE LO PIERDAS!!!😎</h2>
       <h2>LAS MEJORES PELIS</h2>
+      <h2>SAGAS COMPLETAS DE TUS HISTORIAS FAVORITAS</h2>
     </div>
   )
 }
