a6dc6fdfadef3989a9c2e13676ef6e10bb3c039d
Luma2001 <lumaqui2001@yahoo.com.ar>
2025-01-15 11:29:34 -0300

    [cfg] test enviroment UAT

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
           <h2>NO TE LO PIERDAS!!!😎</h2>
           <h2>LAS MEJORES PELIS</h2>
           <h2>SAGAS COMPLETAS DE TUS HISTORIAS FAVORITAS</h2>
+          <h2>esta es la rama para que pruebe el cliente</h2>
         </div>
     </>
 
